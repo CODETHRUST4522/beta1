1c53a51a9bdc6aa575ffc606032e251623df509c
CODETHRUST4522 <loopnull4522@gmail.com>
2026-07-21 23:54:08 +0530

    Start Phase 2 - Firebase Authentication

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCb-stvbSaVGqTEy68sDHgR1lmfQpIlCmI",
   authDomain: "beta1-845fa.firebaseapp.com",
   projectId: "beta1-845fa",
   storageBucket: "beta1-845fa.firebasestorage.app",
@@ -10,5 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export default app;
